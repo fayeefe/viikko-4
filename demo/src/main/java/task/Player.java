e2027a1d68e1fe5dba7a50c5cd46ebc96dd3efd7
fayeefe <freija.orvokki@gmail.com>
2025-01-28 20:15:23 +0200

    version 0.1

--- demo/src/main/java/task/Player.java
+++ demo/src/main/java/task/Player.java
@@ -1,9 +1,20 @@
 package task;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Player {
     String name;
 
-    public void attack(){
+    public void attack(ArrayList<Monster> monsterList){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Valitse hirviö, johon hyökätä:");
+        for (Monster monster : monsterList) {
+            System.out.println(monsterList.indexOf(monster) + ": " + monster.type + " / " + monster.health + "HP");
+        }
+        String inputValue = scanner.nextLine();
         
+
+        scanner.close();
     }
 }
